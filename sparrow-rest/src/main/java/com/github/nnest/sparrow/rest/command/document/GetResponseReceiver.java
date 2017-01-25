9b787281eec05e61aba5b46e481b3075d475c720
bradwoo8621 <bradwoo8621@gmail.com>
2017-01-25 17:37:36 +0800

    add a receiver to forbid the object mapper copy
    save performance

---
+++ sparrow-rest/src/main/java/com/github/nnest/sparrow/rest/command/document/GetResponseReceiver.java
@@ -0,0 +1,196 @@
+/**
+ * 
+ */
+package com.github.nnest.sparrow.rest.command.document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * since the type of {@linkplain #setDocument(Object)} is dynamic, which means
+ * cannot receive and transform to object directly by jackson. so the receiver
+ * receive the document data as {@linkplain JsonNode}, and translate it to
+ * document.
+ * 
+ * @author brad.wu
+ * @since 0.0.1
+ * @version 0.0.1
+ */
+public class GetResponseReceiver extends GetResponse {
+	private GetResponse get = new GetResponse();
+	private JsonNode jsonNode = null;
+
+	/**
+	 * unwrap and get real {@linkplain GetResponse}
+	 * 
+	 * @return real response
+	 */
+	public GetResponse unwrap() {
+		return this.get;
+	}
+
+	/**
+	 * @return the jsonNode
+	 */
+	public JsonNode getJsonNode() {
+		return jsonNode;
+	}
+
+	/**
+	 * @param jsonNode
+	 *            the jsonNode to set
+	 */
+	@JsonProperty("_source")
+	public void setJsonNode(JsonNode jsonNode) {
+		this.jsonNode = jsonNode;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#getIndex()
+	 */
+	@Override
+	public String getIndex() {
+		return get.getIndex();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setIndex(java.lang.String)
+	 */
+	@Override
+	@JsonProperty("_index")
+	public void setIndex(String index) {
+		get.setIndex(index);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#getType()
+	 */
+	@Override
+	public String getType() {
+		return get.getType();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setType(java.lang.String)
+	 */
+	@Override
+	@JsonProperty("_type")
+	public void setType(String type) {
+		get.setType(type);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#getId()
+	 */
+	@Override
+	public String getId() {
+		return get.getId();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setId(java.lang.String)
+	 */
+	@Override
+	@JsonProperty("_id")
+	public void setId(String id) {
+		get.setId(id);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#getVersion()
+	 */
+	@Override
+	public String getVersion() {
+		return get.getVersion();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setVersion(java.lang.String)
+	 */
+	@Override
+	@JsonProperty("_version")
+	public void setVersion(String version) {
+		get.setVersion(version);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#isFound()
+	 */
+	@Override
+	public boolean isFound() {
+		return get.isFound();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setFound(boolean)
+	 */
+	@Override
+	public void setFound(boolean found) {
+		get.setFound(found);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#isSuccessful()
+	 */
+	@Override
+	public boolean isSuccessful() {
+		return get.isSuccessful();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#getDocument()
+	 */
+	@Override
+	public <T> T getDocument() {
+		return get.getDocument();
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see com.github.nnest.sparrow.rest.command.document.GetResponse#setDocument(java.lang.Object)
+	 */
+	@Override
+	public void setDocument(Object document) {
+		get.setDocument(document);
+	}
+
+	/**
+	 * transform document
+	 * 
+	 * @param mapper
+	 *            mapper
+	 * @param documentType
+	 *            document type
+	 * @throws JsonProcessingException
+	 */
+	public void transformDocument(ObjectMapper mapper, Class<?> documentType) throws JsonProcessingException {
+		this.setDocument(mapper.treeToValue(this.getJsonNode(), documentType));
+	}
+}
--- sparrow-rest/src/main/java/com/github/nnest/sparrow/rest/command/document/RestCommandGet.java
+++ sparrow-rest/src/main/java/com/github/nnest/sparrow/rest/command/document/RestCommandGet.java
@@ -3,16 +3,9 @@
  */
 package com.github.nnest.sparrow.rest.command.document;
 
-import java.io.IOException;
 import java.io.InputStream;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.github.nnest.sparrow.ElasticCommandKind;
 import com.github.nnest.sparrow.ElasticDocumentDescriptor;
 import com.github.nnest.sparrow.ElasticExecutorException;
@@ -40,12 +33,10 @@ public class RestCommandGet extends AbstractRestCommand<Get, GetResponse> {
 	protected GetResponse readResponse(Get command, InputStream stream) throws ElasticExecutorException {
 		try {
 			ElasticDocumentDescriptor documentDescriptor = command.getDocumentDescriptor();
-			ObjectMapper mapper = this.createResponseObjectMapper(this.getResponseClass()).copy();
-			SimpleModule module = new SimpleModule();
-			module.addDeserializer(GetResponse.class, new GetResponseDeserializer(mapper, documentDescriptor));
-			mapper.registerModule(module);
-			GetResponse response = mapper.readValue(stream, GetResponse.class);
-
+			ObjectMapper mapper = this.createResponseObjectMapper(GetResponseReceiver.class);
+			GetResponseReceiver response = (GetResponseReceiver) this
+					.completeResponse(mapper.readValue(stream, GetResponseReceiver.class), command);
+			response.transformDocument(mapper, documentDescriptor.getDocumentClass());
 			// set id value when it is null, use value of "_id"
 			this.setIdValueIfNull(response.getDocument(), documentDescriptor, new IdDetective() {
 				/**
@@ -58,7 +49,7 @@ public class RestCommandGet extends AbstractRestCommand<Get, GetResponse> {
 					return response.getId();
 				}
 			});
-			return response;
+			return response.unwrap();
 		} catch (Exception e) {
 			throw new ElasticExecutorException("Fail to read data from response.", e);
 		}
@@ -92,72 +83,5 @@ public class RestCommandGet extends AbstractRestCommand<Get, GetResponse> {
 		// use GET for id given
 		request.setMethod(ElasticRestMethod.GET.name());
 		return request;
-	}
-
-	/**
-	 * get response deserializer
-	 * 
-	 * @author brad.wu
-	 * @since 0.0.1
-	 * @version 0.0.1
-	 */
-	public static class GetResponseDeserializer extends StdDeserializer<GetResponse> {
-		private static final long serialVersionUID = 2100430478383947559L;
-		private ObjectMapper mapper = null;
-		private ElasticDocumentDescriptor documentDescriptor = null;
-
-		public GetResponseDeserializer(ObjectMapper mapper, ElasticDocumentDescriptor documentDescriptor) {
-			this(null);
-
-			this.mapper = mapper;
-			this.documentDescriptor = documentDescriptor;
-		}
-
-		private GetResponseDeserializer(Class<?> vc) {
-			super(vc);
-		}
-
-		/**
-		 * @return the mapper
-		 */
-		public ObjectMapper getMapper() {
-			return mapper;
-		}
-
-		/**
-		 * @return the documentDescriptor
-		 */
-		public ElasticDocumentDescriptor getDocumentDescriptor() {
-			return documentDescriptor;
-		}
-
-		/**
-		 * @return the documentType
-		 */
-		public Class<?> getDocumentType() {
-			return this.getDocumentDescriptor().getDocumentClass();
-		}
-
-		/**
-		 * (non-Javadoc)
-		 * 
-		 * @see com.fasterxml.jackson.databind.JsonDeserializer#deserialize(com.fasterxml.jackson.core.JsonParser,
-		 *      com.fasterxml.jackson.databind.DeserializationContext)
-		 */
-		@Override
-		public GetResponse deserialize(JsonParser parser, DeserializationContext ctxt)
-				throws IOException, JsonProcessingException {
-			GetResponse resp = new GetResponse();
-
-			JsonNode node = parser.getCodec().readTree(parser);
-			resp.setIndex(node.get("_index").asText());
-			resp.setType(node.get("_type").asText());
-			resp.setId(node.get("_id").asText());
-			resp.setVersion(node.get("_version").asText());
-			resp.setFound(node.get("found").asBoolean());
-			resp.setDocument(mapper.treeToValue(node.get("_source"), this.getDocumentType()));
-
-			return resp;
-		}
 	}
 }
